5889ecde5fe9d401ccad07ecd1720e21f0f7047b
Jai Juan Solis <solisjm@miamioh.edu>
2022-05-04 09:50:24 -0400

    Solution

---
+++ src/Location.php
@@ -0,0 +1,57 @@
+<?php 
+
+/*
+ * Jai Juan Solis
+ * - using Kyle Lierer's solution
+ *
+ * (c) Kyle Lierer <liererkt@miamioh.edu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace liererkt\MyFirstPackage;
+
+/**
+ * Represents an OpenWeather location.
+ */
+class Location
+{
+    private $city;
+    private $state;
+    private $country;
+
+    /**
+     * Creates a new Location.
+     */
+    public function __construct($city, $state, $country)
+    {
+        $this->city = $city;
+        $this->state = $state;
+        $this->country = $country;
+    }
+
+    /**
+     * Returns the city.
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Returns the state.
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Returns the country.
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
